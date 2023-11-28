8ccb8be39dcfc235febf6f33391334330def26fb
Tymur Kosiak <us1gav@gmail.com>
2023-11-28 02:55:24 +0200

    NewsApiProxyRestClient create a project.

---
+++ src/main/java/edu/hillel/newsapiproxyrestclient/NewsApiProxyRestClientApplication.java
@@ -0,0 +1,13 @@
+package edu.hillel.newsapiproxyrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiProxyRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApiProxyRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hillel/newsapiproxyrestclient/NewsApiProxyRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hillel.newsapiproxyrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiProxyRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
